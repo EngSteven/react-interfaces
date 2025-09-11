50f3a33c84b8c73bf886aef5f83d31ff5cfd77c9
Steven Sequeira <stesequeira11@gmail.com>
2025-09-11 12:15:21 -0600

    first commit

---
+++ chatgpt-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <h1>Hello, this is Home Page!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
